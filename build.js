cd29cc4119e7db47f3ef4ed6c5745b20aafb7a92
James Eschrich <jaeschrich@gmail.com>
2021-03-26 11:47:46 -0400

    starting merge

---
+++ build.js
@@ -0,0 +1,37 @@
+const { join } = require('path');
+const esbuild = require('esbuild');
+const autoprefixer = require("autoprefixer");
+const postCssPlugin = require("esbuild-plugin-postcss2").default;
+
+const frontendEntry = join(__dirname, 'frontend', 'client.tsx')
+const submitFormEntry = join(__dirname, 'submit-form', 'main.tsx')
+const submitFormCSS = join(__dirname, 'submit-form', 'submit.css')
+
+function build(overrideOptions = {}) {
+    return esbuild.build({
+        entryPoints: [ frontendEntry, submitFormEntry ],
+        bundle: true,
+        outdir: 'dist',
+        sourcemap: true,
+        logLevel: 'info',
+        logLimit: 0,
+        loader: { '.svg': 'dataurl' },
+        plugins: [
+            postCssPlugin({
+                plugins: [ autoprefixer, atImport ]
+            })
+        ],
+        define: {
+            'process.env.NODE_ENV': '"development"'
+        },
+        ...overrideOptions
+    })
+}
+
+if (require.main === module) {
+    const watch = process.argv.indexOf('--watch') > -1
+    build({ watch }).catch(e => {
+        console.log(e.message)
+        process.exit(1)
+    });
+}
