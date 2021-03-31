808d6fc1e50e3dc3834f3e236b3885db4fed2765
jprome <josepaulrc145@gmail.com>
2021-03-31 17:29:23 -0400

    Creates some routes and models

--- backend/models/user.ts
+++ backend/models/user.ts
@@ -1,4 +1,4 @@
-import { IUser } from "../types/user"
+import { IUser } from "../types/types"
 import { model, Schema } from "mongoose"
 
 const userSchema: Schema = new Schema(
@@ -13,10 +13,46 @@ const userSchema: Schema = new Schema(
     },
     role: {
       type: Number,
+      required: true, // 0 author / 1 staff / 2 editor / editor-in-chief
+    },
+    penName: {
+      type: String,
       required: true,
     },
+    intro: {
+      type: String,
+      required: false
+    },
+    content: [
+      {
+        link: {type: String, required: true},
+        comment: {type: String, required: true}
+      }]
+    , 
+    submissions: {
+      type: Array,
+      required: false
+    },
+    requested_edit_submissions: {
+      type: Array,
+      required: false
+    },
+    submissions_array_vote: {
+      type: Array,
+      required: false
+    },
+    vote_log : {
+      type: Array,
+      required: false
+    }
   },
   { timestamps: true }
 )
 
 export default model<IUser>("User", userSchema)
+
+
+
+//vote_log:Array<[String,number]> // submission, vote
+//accept_reject_array: Array<string>
+
