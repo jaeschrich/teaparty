343fab76a45fd74bc1e259d02f5857731ac29c16
James Eschrich <jaeschrich@gmail.com>
2021-04-02 17:25:57 -0400

    small change to author page

--- frontend/app/components/AuthorPage.tsx
+++ frontend/app/components/AuthorPage.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useParams } from 'react-router';
 import { SubmissionView } from '../submissions';
 
-export function AuthorPage({ name } : { name: string }) {
+export function AuthorPage({  } : {  }) {
+    const { name } = useParams<{ name : string }>();
     return (<div>
         <p>{name}'s page</p>
         {/* <SubmissionView /> -- use this to display each submission */}
