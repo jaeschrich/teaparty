3807379164ef573e2434f0e8d4bb6d60847d8ed8
Azuredc <dariancheung18@hotmail.com>
2021-04-21 13:03:34 -0400

    Comment cleanup and spelling

--- frontend/app/components/AuthorPieces.tsx
+++ frontend/app/components/AuthorPieces.tsx
@@ -6,15 +6,11 @@ import { Submission } from '../state';
 
 export const AuthorPieces = observer(({ submissions } : { submissions: Submission[] }) => {
 
-    //Note: Need connection from backend to set the name to the page and pull his/her art pieces from database
-
     const { id } = useParams<{ id : string }>();
     const subs = submissions.filter((sub: any) => sub.author.id === id)
     const heading = (subs.length)?(<h1>{subs[0].authorName}'s Submissions</h1>):"";
     let nodes = subs.map((sub: any) => 
-            <SubmissionView key={sub.id} value={sub} />);
-    
-    //May need to make a different Array.map function to only have the pieces and not the author
+            <SubmissionView key={sub.id} value={sub} />);   
 
     return (
         
@@ -23,8 +19,6 @@ export const AuthorPieces = observer(({ submissions } : { submissions: Submissio
             <div style={{ borderTop: "0.1rem solid #CCC" }}>
                 {nodes}
             </div>
-            {/* <p>{nodes}</p> */}
-            {/* <SubmissionView /> -- use this to display each submission */}
         </div>
         
     )
